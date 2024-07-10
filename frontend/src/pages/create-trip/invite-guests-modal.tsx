18eb63ab08bb35dcf6cb35f39e6690e1dd736cb0
Marcos Filipe Capella <marcoscapella@outlook.com>
2024-07-10 18:12:28 -0300

    Created a global component for Button and applied it wherever possible

--- frontend/src/pages/create-trip/invite-guests-modal.tsx
+++ frontend/src/pages/create-trip/invite-guests-modal.tsx
@@ -1,5 +1,6 @@
 import { AtSign, Plus, X } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 /**
  * The interface is a great way for me to guarantee the type of the props that I'm receiving.
@@ -74,12 +75,11 @@ export function InviteGuestsModal({
               className="bg-transparent placeholder-zinc-400 outline-none flex-1"
             />
           </div>
-          <button
-            type="submit"
-            className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+
+          <Button type="submit" variant="primary">
             Convidar
             <Plus className="size-5" />
-          </button>
+          </Button>
         </form>
       </div>
     </div>
