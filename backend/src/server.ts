be3f88084cad76176aeb1a993e6e413c1a1cbc1a
Marcos Filipe Capella <marcoscapella@outlook.com>
2024-07-12 21:08:36 -0300

    Created schemas for trips and participants. Created email sender with nodemailer

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { createTrip } from "./routes/create-trip";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { confrimTrip } from "./routes/confirm-trip";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(cors, {
+  origin: "*", // Qualquer endereço front-end pode acessar a minha aplicação
+});
+
+app.register(createTrip);
+app.register(confrimTrip);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running");
+});
