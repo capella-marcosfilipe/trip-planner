bd935f48f8ead934c05748dff3cf2d4235e2710a
Marcos Filipe Capella <marcoscapella@outlook.com>
2024-07-18 21:47:18 -0300

    Created route for creating invite

---
+++ backend/src/routes/trips/create-invite.ts
@@ -0,0 +1,78 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import nodemailer from "nodemailer";
+import z from "zod";
+import { dayjs } from "../../lib/dayjs";
+import { getMailClient } from "../../lib/mail";
+import { prisma } from "../../lib/prisma";
+
+export async function createInvite(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips/:tripId/invites",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          email: z.string().email(),
+        }),
+      },
+    },
+    async (req) => {
+      const { tripId } = req.params;
+      const { email } = req.body;
+
+      const trip = await prisma.trip.findUnique({
+        where: { id: tripId },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found");
+      }
+
+      const participant = await prisma.participant.create({
+        data: {
+          email,
+          trip_id: tripId,
+        },
+      });
+
+      // Enviar e-mail para que os participantes confirmem
+      const formattedStartDate = dayjs(trip.starts_at).format("LL");
+      const formattedEndDate = dayjs(trip.ends_at).format("LL");
+
+      const mail = await getMailClient();
+
+      // Método all aguarda várias promises terminarem. Vou enviar uma array de promises
+      // O map cria uma array e vou criar promises com async
+      const confirmationLink = `https://localhost:3333/participants/${participant.id}/confirm`;
+
+      const message = await mail.sendMail({
+        from: {
+          name: "Equipe plann.er",
+          address: "contato@plann.er",
+        },
+        to: participant.email,
+        subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
+        html: `
+          <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+            <p>Você foi convidado para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formattedStartDate} até ${formattedEndDate}</strong></p>
+            <p></p>
+            <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
+            <p></p>
+            <p>
+              <a href="${confirmationLink}">Confirmar viagem</a>
+            </p>
+            <p></p>
+            <p>Caso você não saiba do que se trata esse e-mail ou não poderá estar presente, apenas ignore esse e-mail.</p>
+          </div>
+        `.trim(),
+      });
+
+      console.log(nodemailer.getTestMessageUrl(message));
+
+      return { participantId: participant.id };
+    }
+  );
+}
